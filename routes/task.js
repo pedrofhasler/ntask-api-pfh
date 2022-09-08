a6b9880a3bf82c7bec3b008f91885fecfc8baa3d
pedrofhasler <pedro.hasler@gmail.com>
2022-09-08 15:32:56 -0300

    feat: Task CRUD done

--- routes/task.js
+++ routes/task.js
@@ -1,12 +1,59 @@
 module.exports = (app) => {
   const Tasks = app.models.tasks;
 
-  app.get('/task', async (req, res) => {
-    try {
-      const tasks = await Tasks.findAll();
-      res.json({ tasks });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+  app
+    .route('/task')
+    .get(async (req, res) => {
+      try {
+        const result = await Tasks.findAll();
+        res.json(result);
+      } catch (error) {
+        res.status(412).json({ msg: error.message });
+      }
+    })
+    .post(async (req, res) => {
+      try {
+        const result = await Tasks.create(req.body);
+        res.json(result);
+      } catch (error) {
+        res.status(412).json({ msg: error.message });
+      }
+    });
+
+  app
+    .route('/task/:id')
+    .get(async (req, res) => {
+      try {
+        const { id } = req.params;
+        const where = { id };
+        const result = await Tasks.findOne({ where });
+        if (result) {
+          res.json(result);
+        } else {
+          res.sendStatus(404);
+        }
+      } catch (error) {
+        res.status(412).json({ msg: error.message });
+      }
+    })
+    .put(async (req, res) => {
+      try {
+        const { id } = req.params;
+        const where = { id };
+        await Tasks.update({ where });
+        res.sendStatus(204);
+      } catch (error) {
+        res.status(412).json({ msg: error.message });
+      }
+    })
+    .delete(async (req, res) => {
+      try {
+        const { id } = req.params;
+        const where = { id };
+        await Tasks.destroy({ where });
+        res.sendStatus(204);
+      } catch (error) {
+        res.status(412).json({ msg: error.message });
+      }
+    });
 };
